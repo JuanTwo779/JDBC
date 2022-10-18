00e62f39ef7e4ccac5a85aff2e2ce81123469188
JuanTwo779 <104906032+JuanTwo779@users.noreply.github.com>
2022-10-19 02:33:05 +1100

    UI

---
+++ src/MovieInfo.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class MovieInfo {
+    private JTextField movieTitleText;
+    private JLabel movieTitleLbl;
+    private JLabel titleLbl;
+    private JLabel yearLbl;
+    private JTextField lastNameText;
+    private JButton saveButton;
+    private JTable table1;
+}
