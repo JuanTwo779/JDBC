f3a8bfb129a54979790ae861cd208735e0ce9f1f
JuanTwo779 <104906032+JuanTwo779@users.noreply.github.com>
2022-10-19 16:14:36 +1100

    panels and table

--- src/MovieInfo.java
+++ src/MovieInfo.java
@@ -6,6 +6,34 @@ public class MovieInfo {
     private JLabel titleLbl;
     private JLabel yearLbl;
     private JTextField lastNameText;
-    private JButton saveButton;
-    private JTable table1;
+    private JButton saveAndAddDataButton;
+    private JLabel directorLbl;
+    private JTextField directorText;
+    private JLabel genreLbl;
+    private JTextField genreText;
+    private JLabel ratingLbl;
+    private JTextField ratingText;
+    private JLabel countryLbl;
+    private JTextField countryText;
+    private JRadioButton yesRadioBtn;
+    private JLabel watchLbl;
+    private JRadioButton noRadioBtn;
+    private JPanel rootPanel;
+    private JPanel dataPanel;
+    private JPanel tablePanel;
+    private JScrollPane tableScrollPane;
+    private JTable showTable;
+    private JPanel titlePanel;
+
+    public MovieInfo(){
+        createTable();
+    }
+
+    public JPanel getRootPanel(){
+        return rootPanel;
+    }
+
+    private void createTable(){
+
+    }
 }
