17466867de89e4f4e952ab5e6eb136a4ab58f74e
JuanTwo779 <104906032+JuanTwo779@users.noreply.github.com>
2022-10-24 19:33:29 +1100

    add button method created

--- src/UI/MovieInfo.java
+++ src/UI/MovieInfo.java
@@ -9,22 +9,23 @@ import javax.swing.table.TableColumnModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class MovieInfo implements ActionListener {
+public class MovieInfo  {
     private JPanel rootPanel;
-    private JTextField movieTitleText;
-    private JLabel movieTitleLbl;
-    private JLabel titleLbl;
-    private JLabel yearLbl;
-    private JTextField lastNameText;
-    private JLabel directorLbl;
-    private JTextField directorText;
-    private JLabel genreLbl;
-    private JTextField genreText;
-    private JLabel ratingLbl;
-    private JTextField ratingText;
-    private JLabel countryLbl;
-    private JTextField countryText;
-    private JLabel watchLbl;
+    private JTextField txtMovie;
+    private JLabel lblMovie;
+    private JLabel lblTitle;
+    private JLabel lblYear;
+    private JTextField txtYear;
+    private JLabel lblDirector;
+    private JTextField txtDirector;
+    private JLabel lblGenre;
+    private JTextField txtGenre;
+    private JLabel lblRating;
+    private JTextField txtRating;
+    private JLabel lblCountry;
+    private JTextField txtCountry;
+    private JLabel lblWatch;
+    private JTextField txtWatch;
     private JPanel dataPanel;
     private JPanel tablePanel;
     private JScrollPane tableScrollPane;
@@ -34,14 +35,18 @@ public class MovieInfo implements ActionListener {
     private JButton addBtn;
     private JButton editBtn;
     private JButton deleteBtn;
-    private JRadioButton yesRadioButton;
-    private JRadioButton noRadioButton;
 
-    public MovieInfo(){
+    private Object[][] data;
+
+    private Connection con;
+    private Statement st;
+    private PreparedStatement pst; //accepts input params
+
+    public MovieInfo() {
 
         connect();
         createTable();
-        buttonGroup();
+        addBtnAction();
     }
 
     public JPanel getRootPanel(){
@@ -52,33 +57,25 @@ public class MovieInfo implements ActionListener {
     //connection to DB
     public void connect(){
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/movie_data", "****", "****");
-            Statement statement = connection.createStatement();
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/movie_data", "root", "Hansosjsneia-90");
 
             //shows tuples in table on console
-            ResultSet resultSet = statement.executeQuery("select * from movies");
-            while(resultSet.next()){
-                System.out.println(resultSet.getString(2));
-            }
+//            ResultSet resultSet = statement.executeQuery("select * from movies");
+//            while(resultSet.next()){
+//                System.out.println(resultSet.getString(2));
+//            }
         }catch(Exception e){
             e.printStackTrace();
         }
-
-    }
-
-    private void buttonGroup() {
-        ButtonGroup group = new ButtonGroup();
-        group.add(yesRadioButton);
-        group.add(noRadioButton);
     }
 
     private void createTable(){
 
-        Object[][] data = {
-                {"The Dark Knight", 2008, "Bruce Wayne", "Dark", 9.0, "USA", "I"},
-                {"Star Wars: Phantom Menace", 2005, "Wayne", "Sci-fi", 9.2, "USA", "I"},
-                {"Inception", 2015, "Juan", "Thriller", 10, "USA", 0}
-        };
+        data = new Object[][]{};
+
+//        {"The Dark Knight", 2008, "Bruce Wayne", "Dark", 9.0, "USA", "I"},
+//        {"Star Wars: Phantom Menace", 2005, "Wayne", "Sci-fi", 9.2, "USA", "I"},
+//        {"Inception", 2015, "Juan", "Thriller", 10, "USA", 0}
 
         showTable.setModel(new DefaultTableModel(
                 data,
@@ -98,8 +95,74 @@ public class MovieInfo implements ActionListener {
         columns.getColumn(6).setCellRenderer(centerRender);
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
+    private void addBtnAction()
+    {
+        addBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
+                String title, year, director, genre, rating, country, status;
+
+                title = txtMovie.getText();
+                year = txtYear.getText();
+                director = txtDirector.getText();
+                genre = txtGenre.getText();
+                rating = txtRating.getText();
+                country = txtCountry.getText();
+                status = txtWatch.getText();
+
+                try {
+                    pst = con.prepareStatement(
+                            "insert into movies(movie_title, movie_year, movie_dir,movie_gen, " +
+                                    "movie_rat, movie_country, movie_status) values (?,?,?,?,?,?,?)");
+                    pst.setString(1,title);
+                    pst.setString(2,year);
+                    pst.setString(3,director);
+                    pst.setString(4,genre);
+                    pst.setString(5,rating);
+                    pst.setString(6,country);
+                    pst.setString(7,status);
+
+                    pst.executeUpdate();
+                    JOptionPane.showMessageDialog( null, "Movie added");
+
+                    txtMovie.setText("");
+                    txtYear.setText("");
+                    txtDirector.setText("");
+                    txtGenre.setText("");
+                    txtRating.setText("");
+                    txtCountry.setText("");
+                    txtWatch.setText("");
+
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+
+            }
+        });
     }
+
+    private void editBtnAction()
+    {
+        addBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Hello");
+
+            }
+        });
+    }
+
+    private void deleteBtnAction()
+    {
+        addBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Hello");
+
+            }
+        });
+    }
+
+
 }
